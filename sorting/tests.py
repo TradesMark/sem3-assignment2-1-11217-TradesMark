cbe2480332d0ac3119117de4876db3387bd9791b
Anton Galkin <artemis9898@mail.ru>
2017-12-10 15:21:02 +0300

    Update tests.py

--- sorting/tests.py
+++ sorting/tests.py
@@ -1,1 +1,9 @@
+from sort import sort
+import pytest
 
+
+@pytest.mark.parametrize("a, result", [(range(10, 100), ['14', '28', '29', '35', '41',
+                                                         '53', '67', '76', '82', '92'])])
+
+def test_sort(a, result):
+    assert sort(a) == result
