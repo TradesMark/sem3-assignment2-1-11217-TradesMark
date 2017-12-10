203ec523179b534f1e2105fe9ace65c07a057523
Anton Galkin <artemis9898@mail.ru>
2017-12-10 14:58:34 +0300

    Update sort.py

--- sorting/sort.py
+++ sorting/sort.py
@@ -1,1 +1,20 @@
+# Практичское задание NOD
+# Реализуйте два алгоритма сортировки: методом пузырька и быструю сортировку, протестируйте их работу с использованием пакета hypothesis
+# Сделаль Галкин Антон 1 группа 2 курс 
 
+def sort(x):
+  list = []
+  for i in x:
+      i = str(i)
+      s = 0
+      for j in i:
+          s += int(j)**2
+      if 0 == s % 17:
+          list.append(i)
+  return list
+
+def main():
+    print(' Result: ', sort(range(1000,10000)))
+
+if __name__ == '__main__':
+    main()
